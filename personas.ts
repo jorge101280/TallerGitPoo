617880c45963258e34aaeef599ebef6236e9cc5d
jorge101280 <jorge-alejandro-101@hotmail.com>
2024-09-25 10:11:22 -0500

    ejercicio nro 2
    ejercicio nro 2

---
+++ personas.ts
@@ -0,0 +1,14 @@
+//crear clase persona y exportarla con nombre, edad y metodo saludar que imprima “Persona: Tú nombre es xxxx y tienes xx años”
+
+export class Persona {
+    nombre: string;
+    edad: number;
+    constructor(nombre: string, edad: number) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    saludar() {
+        console.log(`Persona: Tú nombre es ${this.nombre} y tienes ${this.edad} años`);
+    }
+}
+
